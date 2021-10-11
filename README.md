a297007dd1ea7713933801de665a6e01d3445057
burglauer2kx <92105397+burglauer2kx@users.noreply.github.com>
2021-10-11 13:29:05 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+- Specify the Truffle version and OpenZeppelin version used in the project.
+
+truffle version
+Truffle v5.4.11 (core: 5.4.11)
+Solidity v0.5.16 (solc-js)
+Node v14.17.6
+Web3.js v1.5.2
+
+npm view openzeppelin-solidity version
+4.3.2
+
+- Your ERC-721 Token Name: My very own star notary token
+- Your ERC-721 Token Symbol: MVOSNT
+- Your “Token Address” on the Rinkeby Network: https://rinkeby.etherscan.io/address/0x6d90a75f8F9cbace5E3b350D7B1d1FAA29585E53
---
+++ truffle-config.js
@@ -0,0 +1,36 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const infuraKey = "3408f1d3449a4a1c941b7db67abd57c2";
+//
+// const fs = require('fs');
+const mnemonic = '...'; //fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+	rinkeby: {
+      provider: function() { 
+       return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/" + infuraKey);
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+	}
+  },
+  compilers: {
+    solc: {
+       version: "0.4.24",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+};
